d693d857540a3426177661415860a4fd84d3480f
frost917 <frost97114@gmail.com>
2021-06-21 13:50:11 +0900

    use cross compile

---
+++ dockerBuild.sh
@@ -0,0 +1,18 @@
+﻿REGISTER=$REGISTER
+gitDir="/home/frost917/mediawiki"
+ver="1.36"
+
+#  build
+cd  ${gitDir}/${ver}/nginx-alpine/
+docker buildx build \
+    --push \
+    --platform=linux/arm64,linux/amd64 \
+    -t ${REGISTER}/mediawiki:${ver}-nginx-alpine \
+    ./ 
+
+cd ${gitDir}/${ver}/fpm-buster/
+docker buildx build \
+    --push \
+    --platform=linux/arm64,linux/amd64 \
+    -t ${REGISTER}/mediawiki:${ver}-fpm-buster \
+    ./
