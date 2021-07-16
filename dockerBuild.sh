bd8b7bd1a581913e7a7c00a7375a744d78f2478c
JungYeon Kim <frost917@frost917.com>
2021-07-16 14:25:12 +0900

    upgrade 1.36 to 1.36.1

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,6 +1,7 @@
 ﻿#!/bin/sh
 gitDir=$(dirname $(realpath $0))
 ver="1.36"
+specVer="1.36.1"
 
 #  build
 cd  ${gitDir}/${ver}/nginx-alpine/
@@ -8,6 +9,7 @@ docker buildx build \
     --push \
     --platform=linux/arm64,linux/amd64 \
     -t ${REGISTRY}/mediawiki:${ver}-nginx-alpine \
+    -t ${REGISTRY}/mediawiki:${specVer}-nginx-alpine \
     ./ 
 
 cd ${gitDir}/${ver}/fpm-buster/
@@ -15,4 +17,5 @@ docker buildx build \
     --push \
     --platform=linux/arm64,linux/amd64 \
     -t ${REGISTRY}/mediawiki:${ver}-fpm-buster \
+    -t ${REGISTRY}/mediawiki:${specVer}-fpm-buster \
     ./
